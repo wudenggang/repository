b32825f69b83aeeef63ea40181b82ceff0f2efd9
wudenggang <wudenggang@outlook.com>
2020-06-11 21:22:22 +0800

    init

---
+++ test.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+
+def callback(*event):
+    print(s.get())
+
+
+root = tk.Tk()
+tk.Label(root,text="输入：").pack(side="left")
+s = tk.StringVar(root)
+ent = tk.Entry(root,textvariable=s)
+ent.pack(side="left")
+s.trace("w", callback)
+root.mainloop()
+
